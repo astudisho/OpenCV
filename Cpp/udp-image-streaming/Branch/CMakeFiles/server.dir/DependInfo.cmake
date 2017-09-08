112a72dc550c1e6edf75bdbae1009d647664ce4a
Javier Astudillo <astudillo@outlook.com>
2017-09-08 00:57:29 -0500

    Added UDP stream

---
+++ Cpp/udp-image-streaming/Branch/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/astudillo/Documents/OpenCv/Cpp/udp-image-streaming/Branch/PracticalSocket.cpp" "/home/astudillo/Documents/OpenCv/Cpp/udp-image-streaming/Branch/CMakeFiles/server.dir/PracticalSocket.cpp.o"
+  "/home/astudillo/Documents/OpenCv/Cpp/udp-image-streaming/Branch/Server.cpp" "/home/astudillo/Documents/OpenCv/Cpp/udp-image-streaming/Branch/CMakeFiles/server.dir/Server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
